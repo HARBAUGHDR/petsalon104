01858c73ddbae9877d19ee732cd6f8a70fe9489e
HARBAUGHDR <78834499+HARBAUGHDR@users.noreply.github.com>
2024-09-21 03:39:27 -0700

    propoerty and function

--- Scripts/register.js
+++ Scripts/register.js
@@ -1,25 +1,47 @@
-console.log("Register");
 let pets=[];//empty array
+// create the Constructor
+//<-------parameters are local------>
+function Pet(name,age,gender,breed,color,temperment,weight){
+    //property=parameters
+    this.name=name;
+    this.age=age;
+    this.gender=gender;
+    this.breed=breed;
+    this.color=color;
+    this.temperment=temperment;
+    this.weight=weight;
+}
 
 //get the inputs
-let inputName = document.getElementById("txtName")
-let inputAge = document.getElementById("txtAge")
-let inputGender = document.getElementById("txtGender")
-let inputTemperment = document.getElementById("txtTemperment")
-let inputWeight = document.getElementById("txtWeight")
-let inputColor = document.getElementById("txtColor")
+let inputName = document.getElementById("txtName");
+let inputAge = document.getElementById("txtAge");
+let inputGender = document.getElementById("txtGender");
+let inputTemperment = document.getElementById("txtTemperment");
+let inputWeight = document.getElementById("txtWeight");
+let inputColor = document.getElementById("txtColor");
 
 function register(){
     console.log("Register function");
     console.log(inputName.value,inputWeight.value,inputColor.value,inputAge.value,inputGender.value,inputBreed.value,inputTemperment.value)
 //creat an obj
-//get the values 
+let newPet = new Pet(inputName.value,inputWeight.value,inputColor.value,inputAge.value,inputGender.value,inputBreed.value,inputTemperment.value);
+pets.push(newPet)
 //display on the console the new pet
+console.log(newPet); 
 }
 
 
 function init(){
     console.log("Init fn");//start the carousel of images
+    let Gamora = new Pet("Gamora,75,Brindle,2,female,German,playful");
+    pets.push(Gamora);
+    let Thanos = new Pet("Thanos,100,Brindle,6,German,Timid");
+    pets.push(Thanos);
+    let Ryder = new Pet("Ryder,45,Black,13,Beagle,Happy");
+    pets.push(Ryder);
+    let Nebula = new Pet("Nebula,45,Merle,3,Aussie,Energetic");
+    pets.push(Nebula);
+    console.log("init fn");
 }
 
 
